0261c9d67a018980a9e934b1385d708ff8b01bf5
Lee Gilbert <lee.gilbert@live.com>
2019-01-17 18:41:44 +1300

    updated TalkDate Stream Util test

--- ltk/src/test/java/com/leeGilbert/ltk/util/TalkDateStreamUtilTest.java
+++ ltk/src/test/java/com/leeGilbert/ltk/util/TalkDateStreamUtilTest.java
@@ -1,12 +1,24 @@
 package com.leeGilbert.ltk.util;
 
+import org.junit.Assert;
 import org.junit.Test;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
 public class TalkDateStreamUtilTest {
 
     @Test
-    public void newTalkDateStream() {
+    public void TalkDateStreamContainsTuesdays() { //TODO add more asserts
+        LocalDate today = LocalDate.now();
+        TalkDateStreamUtil.newTalkDateStream(today)
+                .limit(6)
+                .forEach(d -> {
+                    Assert.assertThat(d.isBefore(today), is(false)) ;
+                    Assert.assertThat(d.getDayOfWeek(), is(DayOfWeek.TUESDAY)) ;
+                });
     }
 }
