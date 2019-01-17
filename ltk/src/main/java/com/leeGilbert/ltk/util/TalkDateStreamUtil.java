091c6bfa7b2af41622a5d9c6002d9e05bf105577
Lee Gilbert <lee.gilbert@live.com>
2019-01-17 18:40:47 +1300

    added TalkDate Stream Util

---
+++ ltk/src/main/java/com/leeGilbert/ltk/util/TalkDateStreamUtil.java
@@ -0,0 +1,61 @@
+package com.leeGilbert.ltk.util;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+import static java.time.temporal.TemporalAdjusters.firstInMonth;
+
+/**
+ * Talk Date util, with no Holiday calendar.
+ */
+public final class TalkDateStreamUtil {
+
+    /**
+     * Returns an Infinite Stream of First Tuesday of Even Months, with no observed Holiday calendar.
+     * Usage:    TalkDateStreamUtil.newTalkDateStream(LocalDate.now())
+     *                 .limit(6)
+     *                 .forEach(System.out::println);
+     * @param start LocalDate  from which to stream
+     * @return Stream<LocalDate> Of talk days
+     */
+    public static Stream<LocalDate> newTalkDateStream(LocalDate start) {
+        LocalDate nextTalkDate = calcNextTalkDate(start);
+        final AtomicLong vEpoch = new AtomicLong(nextTalkDate.toEpochDay());
+        Stream<LocalDate> localDateStream = Stream.generate(() -> vEpoch.getAndSet(epochAdd2months.apply(vEpoch.get())))
+                .map(x -> calcNextTalkDate(x));
+        return localDateStream;
+    }
+
+    private static final Function<Long, Long> epochAdd2months = epoch -> LocalDate.ofEpochDay(epoch).plusMonths(2).withDayOfMonth(1).toEpochDay();
+
+    private static LocalDate calcNextTalkDate(Long startFromEpoch) {
+        return calcNextTalkDate(LocalDate.ofEpochDay(startFromEpoch));
+    }
+
+    private static  LocalDate calcNextTalkDate(LocalDate startFrom) {
+        LocalDate startDT = startFrom;
+        if (!isEvenMonth(startDT))  {
+            startDT = startDT.plusMonths(1);
+        }
+        LocalDate firstTuesdayInMonth = calcFirstDayInMonth(startDT, DayOfWeek.TUESDAY);
+        if (!firstTuesdayInMonth.isEqual(startFrom) && !firstTuesdayInMonth.isAfter(startFrom)) {
+            startDT = startDT.plusMonths(1);
+        }
+        firstTuesdayInMonth = calcFirstDayInMonth(startDT, DayOfWeek.TUESDAY);
+        return firstTuesdayInMonth;
+    }
+
+    private static  boolean isEvenMonth(LocalDate today) {
+        return today.getMonthValue() % 2 == 0;
+    }
+
+
+    private  static LocalDate calcFirstDayInMonth(LocalDate dt, DayOfWeek dayOfWeek) {
+        return dt.with(firstInMonth(dayOfWeek)).atStartOfDay().toLocalDate();
+    }
+
+
+}
---
+++ ltk/src/test/java/com/leeGilbert/ltk/util/TalkDateStreamUtilTest.java
@@ -0,0 +1,12 @@
+package com.leeGilbert.ltk.util;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TalkDateStreamUtilTest {
+
+    @Test
+    public void newTalkDateStream() {
+    }
+}
