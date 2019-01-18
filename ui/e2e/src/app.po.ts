45dba391c47b73a164024de447d09d1ff54bb69e
Lee Gilbert <lee.gilbert@live.com>
2019-01-19 10:03:44 +1300

    ng new ui -—routing --style=scss --prefix ltk --skip-git

---
+++ ui/e2e/src/app.po.ts
@@ -0,0 +1,11 @@
+import { browser, by, element } from 'protractor';
+
+export class AppPage {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getTitleText() {
+    return element(by.css('ltk-root h1')).getText();
+  }
+}
