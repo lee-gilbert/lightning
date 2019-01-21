d082131ad5f5f395bc8808d1e768e3d7b8331f5e
Lee Gilbert <lee.gilbert@live.com>
2019-01-21 23:48:26 +1300

    Reactive Form validation for add proposal

--- ui/src/app/proposal-add/proposal-add.component.ts
+++ ui/src/app/proposal-add/proposal-add.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {Router} from '@angular/router';
 import {BackendApiService} from '../services/backend-api.service';
 
@@ -15,7 +15,22 @@ export class ProposalAddComponent implements OnInit {
 
   addForm: FormGroup;
 
-  ngOnInit() {
+  validateAllFormFields(formGroup: FormGroup) {
+  Object.keys(formGroup.controls).forEach(field => {
+    const control = formGroup.get(field);
+    if (control instanceof FormControl) {
+      control.markAsTouched({ onlySelf: true });
+    } else if (control instanceof FormGroup) {
+      this.validateAllFormFields(control);
+    }
+  });
+}
+
+//In case you need to handle validation messages for the FormControls as well, you can modify the code to also mark the
+// FormGroup as touched by simply removing the { onlySelf: true } parameter. When we pass onlSelf: true to the markAsDirty
+//or markAsTouch (or other markAs* methods) Angular only marks the control itself. Without this option, Angular will mark the control and its parent.
+
+ngOnInit() {
     this.addForm = this.formBuilder.group({
       id: [],
       topic: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(80)]],
@@ -25,9 +40,14 @@ export class ProposalAddComponent implements OnInit {
   }
 
   onSubmit() {
+    if (this.addForm.valid) {
     this.apiService.createProposal(this.addForm.value)
       .subscribe( data => {
         this.router.navigate(['proposal-list']);
       });
+  } else {
+    this.validateAllFormFields(this.addForm);
   }
+}
+
 }
