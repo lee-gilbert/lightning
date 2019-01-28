4d7019e1e9c90f343758fc8480955075f25b785e
Lee Gilbert <lee.gilbert@live.com>
2019-01-28 17:11:01 +1300

    number to string

--- ui/src/app/model/api.response.ts
+++ ui/src/app/model/api.response.ts
@@ -1,5 +1,12 @@
 export class ApiResponse {
+
   status: number;
-  message: number;
+  message: string;
   result: any;
+
+  constructor (status: number, message: string, result: any) {
+    this.status = status;
+    this.message = message;
+    this.result = result;
+ }
 }
