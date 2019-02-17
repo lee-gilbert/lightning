bece50ba9dd4a7aad1195f64e4c96e4b44f4f83f
Lee Gilbert <lee.gilbert@live.com>
2019-02-17 13:37:00 +1300

    cors & list beans

--- ltk/src/main/java/com/leeGilbert/ltk/LightningtalksApplication.java
+++ ltk/src/main/java/com/leeGilbert/ltk/LightningtalksApplication.java
@@ -8,18 +8,42 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
 
 
 @SpringBootApplication(exclude = ErrorMvcAutoConfiguration.class )
 public class LightningtalksApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LightningtalksApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(LightningtalksApplication.class, args);
+
+
+		System.out.println("Beans provided by Spring Boot:");
+
+		String[] beanNames = ctx.getBeanDefinitionNames();
+		Arrays.sort(beanNames);
+		for (String beanName : beanNames) {
+			System.out.println(beanName);
+		}
 	}
 
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
+	}
 
 	@Bean
 	CommandLineRunner init(LightningTalksService lts) {
