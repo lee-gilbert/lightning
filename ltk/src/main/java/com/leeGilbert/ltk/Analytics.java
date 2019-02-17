e15ae9216098388f7778cfa5b6b704ed95a57cd6
Lee Gilbert <lee.gilbert@live.com>
2019-02-17 13:43:59 +1300

    support analytics via http to logstash

--- ltk/src/main/java/com/leeGilbert/ltk/Analytics.java
+++ ltk/src/main/java/com/leeGilbert/ltk/Analytics.java
@@ -1,8 +1,67 @@
 package com.leeGilbert.ltk;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 
-@ConfigurationProperties(prefix = "analytics")
-public class AnalyticsProps {
-    String logstashURL;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import javax.annotation.PostConstruct;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.Collections;
+
+@Component
+@Data
+@Validated
+@Slf4j
+public class Analytics {
+
+    @NotBlank
+    private String logstashURL;
+
+    @NotNull
+    private Boolean enabled;
+
+    @NotNull
+    private WebClient client = buildClient();
+
+
+    public Analytics(@Value("${analytics.logstashURL}") String logstashURL, @Value("${analytics.enabled}") Boolean enabled) {
+        this.logstashURL = logstashURL;
+        this.enabled = enabled;
+    }
+
+    private WebClient buildClient() {
+        return WebClient
+                .builder()
+                .baseUrl(logstashURL)
+                //.defaultCookie("cookieKey", "cookieValue")
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .defaultUriVariables(Collections.singletonMap("url", logstashURL))
+                .build();
+    }
+
+    public void logJson(String json) {
+        if (enabled) {
+             client.post().body(BodyInserters.fromObject(json))
+            .exchange()
+            .subscribe(
+                    value -> log.debug("call returned {}", value),
+                    error -> log.error("call returned {}",  error.getMessage()),
+                    () -> log.warn("completed without a value")
+            );
+        }
+    }
+
+
+    @PostConstruct
+    public void init() {
+    }
+
 }
--- ltk/src/test/java/com/leeGilbert/ltk/PropertyTest.java
+++ ltk/src/test/java/com/leeGilbert/ltk/PropertyTest.java
@@ -1,4 +1,34 @@
 package com.leeGilbert.ltk;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.ConfigFileApplicationContextInitializer;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+
+import static org.hamcrest.core.IsNull.notNullValue;
+
+@SpringBootTest
+@TestPropertySource(properties = { "spring.config.location=classpath:application.yml" })
+@ContextConfiguration(classes = { Analytics.class },  initializers = {ConfigFileApplicationContextInitializer.class})
+@RunWith(SpringRunner.class)
+@Slf4j
 public class PropertyTest {
+
+    @Autowired
+    private Analytics analytics;
+
+    @Test
+    public void TestAnalytics() {
+        Assert.assertThat("analytics not null " , analytics, notNullValue());
+        Assert.assertThat("analytics.getEnabled() not null " , analytics.getEnabled(), notNullValue());
+    }
 }
